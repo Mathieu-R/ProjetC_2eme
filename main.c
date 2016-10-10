d9a74b22348d7d5db97d4eaad4cca748bedebe23
Mathieu-R <mathieu0709@gmail.com>
2016-10-10 13:42:07 +0200

    Time Generator

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int genTime(const int max) {
+	return rand() % max + 1; // Generate number between 0 and max
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
