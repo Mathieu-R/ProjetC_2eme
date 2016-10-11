9d845211daef93fa41f523a619efcd3e3b53fa0d
Mathieu-R <mathieu0709@gmail.com>
2016-10-11 10:39:25 +0200

    test shared memory

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int genTime(const int max) {
 	return rand() % max + 1; // Generate number between 0 and max
@@ -7,6 +9,18 @@ int genTime(const int max) {
 
 int main(int argc, char const *argv[])
 {
+	key_t key = 5; // Clé
+	size_t count = 1024; // Taille
+	int perm = 0644; // Permissions
+
+	int shmid = 0; // Shared Memory ID
+
+	shmid = shmget(key, count, 0644);
+
+	if (shmid == -1) {
+		printf("ERREUR: BAD SHARED MEMORY ALLOCATION.");
+		return 0;
+	}
 	
 	return 0;
 }
