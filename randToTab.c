e9c08cfaeb881e6086eeb3aae5d0e860fe73798c
HE201390 <t.dodeur@students.ephec.be>
2016-10-13 12:50:03 +0200

    Update randToTab.c

--- randToTab.c
+++ randToTab.c
@@ -4,9 +4,10 @@
 
 int main()
 {
+    int pool = 20; // nombre de voitures
     int minTime = 600; // temps maximum en secondes
     int maxTime = 720; // temps minimum en secondes
-    int cars[20]; // tab de temps.
+    int cars[pool]; // tab de temps.
     srand ( time(NULL) );
     
     int limitedRand(int low, int high) // Méthode de random basé sur min et max
@@ -15,12 +16,12 @@ int main()
     }
     
 
-    for (int i = 0; i<20; i++) // Remplissage du tableau
+    for (int i = 0; i<pool; i++) // Remplissage du tableau
     {
         cars[i] = limitedRand(minTime, maxTime);
     }
     
-    for (int j = 0; j<20; j++) // affichage (inutile dans le code final)
+    for (int j = 0; j<pool; j++) // affichage (inutile dans le code final)
     {
         printf("%d\n " , cars[j]);
     }
