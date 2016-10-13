3f2f4346ab398f8d094e23cad8e6b9a4897b6cba
HE201390 <t.dodeur@students.ephec.be>
2016-10-13 13:14:29 +0200

    Update randToTab.c

--- randToTab.c
+++ randToTab.c
@@ -5,8 +5,14 @@
 int main()
 {
     int pool = 20; // nombre de voitures
-    int minTime = 600; // temps maximum en secondes
-    int maxTime = 720; // temps minimum en secondes
+    /* autre approche: définir un temps moyen et un taux de déviation par rapport à la moyenne.
+    *  int average = 700; // moyenne de temps
+    *  int deviation = 200; // difference entre average et extremes
+    *  int minTime = average - deviation; // temps minimum en secondes
+    *  int maxTime = average + deviation; // temps maximum en secondes
+    */
+    int minTime = 500; // temps maximum en secondes
+    int maxTime = 900; // temps minimum en secondes
     int cars[pool]; // tab de temps.
     srand ( time(NULL) );
     
