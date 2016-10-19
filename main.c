918250f89145957b016ca4d2b65340b991a0885e
Mathieu-R <mathieu0709@gmail.com>
2016-10-19 14:45:26 +0200

    fix

--- main.c
+++ main.c
@@ -63,13 +63,13 @@ int compare(const void *p1, const void *p2) { // Méthode de comparation pour le
 int run(Pilote *p, char* name) {
 
 	/* Instancie toutes les valeurs (exepté le pilote_id) à 0 */
-	p->s1 = 0;
-	p->bestS1 = 0;
-	p->s2 = 0;
-	p->bestS2 = 0;
-	p->s3 = 0;
-	p->bestS3 = 0;
-	p->best = 0;
+	p->s1 = 3 * 60 * 3600 + 1;
+	p->bestS1 =  3 * 60 * 3600 + 1;
+	p->s2 =  3 * 60 * 3600 + 1;
+	p->bestS2 =  3 * 60 * 3600 + 1;
+	p->s3 =  3 * 60 * 3600 + 1;
+	p->bestS3 =  3 * 60 * 3600 + 1;
+	p->best =  3 * 60 * 3600 + 1;
 	p->isPit = 0;
 	p->hasGivenUp = 0;
 	p->numberOfPits = 0;
@@ -125,25 +125,17 @@ int run(Pilote *p, char* name) {
         p->s3 = S3; // etc...
 
         int lap = S1 + S2 + S3;
+        
 
-        if ((p->best == 0) || (p->bestS1 == 0) || (p->bestS2 == 0) || (p->bestS3 == 0)) { // Si l'on est au premier tours (en effet, au premier tours les bests temps sont initialisés à zéro !)
-        	p->bestS1 = S1; 
-        	p->bestS2 = S2; 
-        	p->bestS3 = S3; 
-
-        	p->best = lap;
-
-        } else {
-
-	        if (p->bestS1 > S1) p->bestS1 = S1; // Si c'est son meilleur S1, on modifie le meilleur s1
-	        if (p->bestS2 > S2) p->bestS2 = S2; // Si c'est son meilleur S2, on modifie le meilleur s2
-	        if (p->bestS3 > S3) p->bestS3 = S3; // Si c'est son meilleur S3, on modifie le meilleur s3
+        if (p->bestS1 > S1) p->bestS1 = S1; // Si c'est son meilleur S1, on modifie le meilleur s1
+        if (p->bestS2 > S2) p->bestS2 = S2; // Si c'est son meilleur S2, on modifie le meilleur s2
+        if (p->bestS3 > S3) p->bestS3 = S3; // Si c'est son meilleur S3, on modifie le meilleur s3
 
 
-	        if (p->best > lap) p->best = lap; // Si c'est son meilleur temps au tour, on le notifie	
-        }
+        if (p->best > lap) p->best = lap; // Si c'est son meilleur temps au tour, on le notifie	
+        
 
-        //printf("%d\n", lap);
+        //printf("%d\n", p->best);
 
         //if (i == MAX_TOURS - 1) exit(-1);
 
@@ -159,18 +151,19 @@ void showResults(struct Pilote tab[], int nbElems) {
 	/*for (int i = 0; i < nbElems; i++) {
 		printf("%d\n", tab[i].best);
 	}*/
+	//delay(10000);
 
 	qsort(tab, nbElems, sizeof(Pilote), compare); 
 
     for (int k = 0; k<nbElems; k++) {
     	
-        printf("%d%s%d%s%d%s%d%s%d%s%d%s\n" ,k+1,": voiture n°", tab[k].pilote_id,": ", tab[k].best/3600,"s (", tab[k].best/(60*3600),"m", (tab[k].best/3600)%60,"s", tab[k].best%60,"ms)"); 
+        printf("%d%s%d%s%d%s%d%s%d%s\n" ,k+1,": voiture n°", tab[k].pilote_id,": (", tab[k].best/(60*3600),"m", (tab[k].best/3600)%60,"s", tab[k].best%60,"ms)"); 
     }
 }
 
 int main(int argc, char const *argv[]) {
 
-    srand ((unsigned) time(NULL));
+    srand (time(NULL));
 
     int pilotes_numbers[MAX_PILOTES]  = {44, 6, 5, 7, 3, 33, 19, 77, 11, 27, 26, 55, 14, 22, 9, 12, 20, 30, 8, 21, 31, 94}; // Tableau contenant les numéro des pilotes
 
@@ -220,67 +213,49 @@ int main(int argc, char const *argv[]) {
                         pilotesTab[j].pilote_id = pilotes_numbers[j]; // Initialise le numéro du pilote
 
                         run(&pilotesTab[j], "Practices");
-
-                        // Affichage
-                        if (j == MAX_PILOTES - 1) {
-                            printf("P%d:\n", i);
-
-                            showResults(pilotesTab, MAX_PILOTES);
-
-                        }
-                    
                     }
+
+                    printf("P%d:\n", i);
+                    showResults(pilotesTab, MAX_PILOTES);
+
                     break;
                 case 4:
 	                for (int j = 0; j < MAX_PILOTES; j++) {
 
                         run(&pilotesTab[j], "Qualifs");
-
-                        // Affichage
-                        if (j == MAX_PILOTES - 1) {
-                            printf("Q1\n");
-
-                            showResults(pilotesTab, MAX_PILOTES);
-
-                            for (int k = 0; k<16; k++) {
-                                Q2[k] = pilotesTab[k];
-                            }
-                        }
                     
+                    }
+
+                    printf("Q1\n");
+                    showResults(pilotesTab, MAX_PILOTES);
+
+                    for (int k = 0; k<16; k++) {
+                        Q2[k] = pilotesTab[k];
                     }
                     break;
                 case 5:
                 	for (int j = 0; j < 16; j++) {
 
                         run(&pilotesTab[j], "Qualifs");
-
-                        // Affichage
-                        if (j == 15) {
-                            printf("Q2\n");
-
-              				showResults(Q2, 16);
-
-                            for (int k = 0; k<10; k++) {
-                                Q3[k] = Q2[k];
-                            }
-                        }
                     
+                    }
+
+                    printf("Q2\n");
+      				showResults(Q2, 16);
+
+                    for (int k = 0; k<10; k++) {
+                        Q3[k] = Q2[k];
                     }
                     break;
                 case 6:
                      for (int j = 0; j < 10; j++) {
 
                         run(&pilotesTab[j], "Qualifs");
-
-                        // Affichage
-                        if (j == 9) {
-                            printf("Q3\n");
-
-                            showResults(Q3, 10);
-
-                        }
                     
                     }
+
+                    printf("Q3\n");
+                    showResults(Q3, 10);
                     break;
                 case 7:
 					 fillTabBeforeRace(); // Crée la grille de départ
@@ -289,15 +264,10 @@ int main(int argc, char const *argv[]) {
 
                         run(&pilotesTab[j], "Race");
 
-                        // Affichage
-                        if (j == MAX_PILOTES - 1) {
-                            printf("Race: \n");
-
-                            showResults(pilotesTab, MAX_PILOTES);
-
-                        }
-                    
                     }
+
+                    printf("Race: \n");
+                    showResults(pilotesTab, MAX_PILOTES);
                     break;
                     
            } 
